eab7efc0573983fdd84398e17c32b83bb83c4b7a
liwi <77789571+dliwilb@users.noreply.github.com>
2022-02-07 16:16:37 +0100

    new website structure

--- script/smartify-assets.js
+++ script/smartify-assets.js
@@ -2,25 +2,6 @@
 // onConnect();
 
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-  
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-        }
-        }
-    }
-}
-
-
 function showInput(inputId, element){
     document.getElementById(inputId).style.display = element.value == '0x0' ? 'block' : 'none';
 }
